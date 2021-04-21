da40f1b32c9572ca2258c8e37a907773847e8a8a
Andrew Burgess <andrew@burgess.io>
2021-04-21 09:14:36 -0400

    AccountNumberParser can parse individual numbers from 0 - 9

--- src/main/AccountNumberParser.ts
+++ src/main/AccountNumberParser.ts
@@ -1,7 +1,15 @@
 import AccountNumberInput from "./AccountNumberInput";
+import * as R from "ramda";
+import Numbers from "./Numbers";
 
 export default class AccountNumberParser {
   parse(input: AccountNumberInput): number {
+    for (let num of Object.keys(Numbers)) {
+      if (R.equals(input, Numbers[num])) {
+        return parseInt(num, 10);
+      }
+    }
+
     return 1;
   }
 }
--- src/test/AccountNumberParser.test.ts
+++ src/test/AccountNumberParser.test.ts
@@ -1,12 +1,25 @@
 import AccountNumberInput from "../main/AccountNumberInput";
 import AccountNumberParser from "../main/AccountNumberParser";
+import Numbers from "../main/Numbers";
 
 describe("AccountNumberParser", () => {
-  it("should parse a single number", () => {
-    const input: AccountNumberInput = ["   ", "  |", "  |", ""];
+  it.each([
+    [0, Numbers["0"]],
+    [1, Numbers["1"]],
+    [2, Numbers["2"]],
+    [3, Numbers["3"]],
+    [4, Numbers["4"]],
+    [5, Numbers["5"]],
+    [6, Numbers["6"]],
+    [7, Numbers["7"]],
+    [8, Numbers["8"]],
+    [9, Numbers["9"]],
+  ])(
+    "should parse in individual %s",
+    (output: number, input: AccountNumberInput) => {
+      const parser = new AccountNumberParser();
 
-    const parser = new AccountNumberParser();
-
-    expect(parser.parse(input)).toBe(1);
-  });
+      expect(parser.parse(input)).toBe(output);
+    }
+  );
 });
