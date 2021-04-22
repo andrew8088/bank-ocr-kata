8dec5df8dcc5c88b140a21d6b9d2fbb45d969ad3
Andrew Burgess <andrew@burgess.io>
2021-04-21 21:44:59 -0400

    AccountNumberReader can read and split a file with multiple account numbers

--- src/main/AccountNumberReader.ts
+++ src/main/AccountNumberReader.ts
@@ -1,7 +1,18 @@
 import * as fs from "fs";
+import AccountNumberInput from "./AccountNumberInput";
 
 export default class AccountNumberReader {
   read(filepath: string): string[] {
-    return fs.readFileSync(filepath).toString().trim().split("\n");
+    return fs.readFileSync(filepath).toString().split("\n");
+  }
+
+  readFileToInputs(filepath: string): AccountNumberInput[] {
+    const lines = this.read(filepath);
+    const inputs: AccountNumberInput[] = [];
+
+    for (let i = 0; i < lines.length; i += 4) {
+      inputs.push([lines[i], lines[i + 1], lines[i + 2]]);
+    }
+    return inputs;
   }
 }
